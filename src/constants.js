3541c1cf0fd7b629108e1c69a703e978cb805ab7
Justin Richards <richards.justind@gmail.com>
2018-11-29 22:06:02 -0500

    circleci finally works. added production endpoint to constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const apiRoot = process.env.REACT_APP_STAGE === 'dev' ? 'http://localhost:5000/api/' : ''
+export const apiRoot = process.env.REACT_APP_STAGE === 'dev' ? 'http://localhost:5000/api/' : 'http://18.220.245.85/api'
 
 export const ItemTypes = {
     DRAG_AVATAR: "dragAvatar"
