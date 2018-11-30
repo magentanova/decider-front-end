a345b00cf75d2ec968c549cf5ff783ac5bf58ba4
Justin Richards <richards.justind@gmail.com>
2018-11-29 23:00:26 -0500

    changed apiRoot to cloudfront url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const apiRoot = process.env.REACT_APP_STAGE === 'dev' ? 'http://localhost:5000/api/' : 'http://18.220.245.85/api/'
+export const apiRoot = process.env.REACT_APP_STAGE === 'dev' ? 'http://localhost:5000/api/' : 'https://d1cb24c6v2hp4p.cloudfront.net/api/'
 
 console.log('api root: ', apiRoot)
 
