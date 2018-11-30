caea51e4401716381001815347336d8b7814d73b
Justin Richards <richards.justind@gmail.com>
2018-11-29 22:13:52 -0500

    added check to constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 export const apiRoot = process.env.REACT_APP_STAGE === 'dev' ? 'http://localhost:5000/api/' : 'http://18.220.245.85/api'
 
+console.log('api root: ', apiRoot)
+
 export const ItemTypes = {
     DRAG_AVATAR: "dragAvatar"
 }
